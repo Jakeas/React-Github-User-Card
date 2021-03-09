8fb0383365e16e564818bd620fa682f45134d092
Jakeas <JakeASierra@gmail.com>
2021-03-09 18:14:15 -0500

    finished one user and styling -now onto followers

--- github-user-card/src/components/Card.js
+++ github-user-card/src/components/Card.js
@@ -11,11 +11,14 @@ class Card extends React.Component {
                     <h1>GitHub User Card</h1>
                     <h2>{user.name}</h2>
                 <img src={user.avatar_url} alt={user.name}/>
-                <p>{user.location}</p>
-                
-                <p>Followers: {user.followers}</p>
-                <p>Following: {user.following}</p>
-                <p>Twitter Username: {user.twitter_username}</p>
+                <div className="paraContainer">
+                    <p>Location: {user.location}</p>
+                    <p>Public Repositories: {user.public_repos}</p>
+                    <p>Followers: {user.followers}</p>
+                    <p>Following: {user.following}</p>
+                    <p>GitHub Username: {user.login}</p>
+                    <p>Twitter Username: {user.twitter_username}</p>
+                </div>
                 </div>
             </div>
         )
