20cd6f07a61b74585ae82f3adb5bd05df070a9e6
Jakeas <JakeASierra@gmail.com>
2021-03-09 20:59:59 -0500

    finished MVP with followers

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -5,27 +5,69 @@ import Card from './components/Card'
 
  class App extends React.Component {
    state = {
-    user: {}
+    gitUser: {},
+    followers: []
   }
+
+    
+
+
 
   componentDidMount(){
     axios.get('https://api.github.com/users/jakeas')
-      .then((res)=>{
-        console.log(res.data)
-        this.setState({
-          user: res.data //[0]
-        }) 
-      })
-      .catch((err)=>{
-        console.error(err)
-      })
+        .then((res)=>{
+          console.log(res.data)
+          this.setState({
+            gitUser: res.data
+          }) 
+            axios.get('https://api.github.com/users/jakeas/followers')
+              .then((res)=>{
+                console.log("nested", res.data)
+                this.setState({
+                  followers: res.data
+                })
+              })
+                .catch((err)=>{
+                console.log(err)
+              })
+        })
+        .catch((err)=>{
+          console.error(err)
+        })
   }
+
+  // componentDidMount(){
+  //   axios.get('https://api.github.com/users/jakeas')
+  //     .then((res)=>{
+  //       console.log(res.data)
+  //       this.setState({
+  //         users: res.data //[0]
+  //       }) 
+  //     })
+  //     .catch((err)=>{
+  //       console.error(err)
+  //     })
+  //   axios.get('https://api.github.com/users/jakeas/followers')
+  //     .then((res)=>{
+  //       console.log("followers:", res.data)
+  //       this.setState({
+  //         users: res.data 
+  //       }) 
+  //     })
+  //     .catch((err)=>{
+  //       console.error(err)
+  //     })
+  // }
+
     render (){
       
       return(
       <div>
-        
-        <Card user={this.state.user}/>
+        <Card follower={this.state.gitUser} />
+        {this.state.followers.map((follower)=>{
+         return (<Card follower={follower}/>)
+        })}
+         
         
       </div>
       )
--- github-user-card/src/components/Card.js
+++ github-user-card/src/components/Card.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 class Card extends React.Component {
     
-    render() {
-        const { user } = this.props;
+    render(props) {
+        const { follower } = this.props;
         return(
             <div className="container">    
                 <div className="card">
 
                     <h1>GitHub User Card</h1>
-                    <h2>{user.name}</h2>
-                <img src={user.avatar_url} alt={user.name}/>
-                <div className="paraContainer">
-                    <p>Location: {user.location}</p>
-                    <p>Public Repositories: {user.public_repos}</p>
-                    <p>Followers: {user.followers}</p>
-                    <p>Following: {user.following}</p>
-                    <p>GitHub Username: {user.login}</p>
-                    <p>Twitter Username: {user.twitter_username}</p>
-                </div>
+                    <h2>{follower?.name || follower?.login}</h2>
+                <img src={follower?.avatar_url} alt={follower?.name}/>
+                {follower.name && <div className="paraContainer">
+                    <p>Location: {follower.location}</p>
+                    <p>Public Repositories: {follower.public_repos}</p>
+                    <p>Followers: {follower.followers}</p>
+                    <p>Following: {follower.following}</p>
+                    <p>GitHub Username: {follower.login}</p>
+                    <p>Twitter Username: {follower.twitter_username}</p>
+                </div>}
                 </div>
             </div>
         )
